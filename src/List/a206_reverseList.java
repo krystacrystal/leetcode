340cd0317cd8e5216be63b4b1a5fd7c09b35755b
chensisi <2532692867@qq.com>
2023-12-27 10:09:27 +0800

    modify

--- src/List/a206_reverseList.java
+++ src/List/a206_reverseList.java
@@ -32,4 +32,29 @@ public class a206_reverseList {
         }
         return pre;
     }
+
+    // 双链表节点
+    public static class DoubleListNode {
+        public int value;
+        public DoubleListNode last;//指向前一个节点
+        public DoubleListNode next;//指向后一个节点
+
+        public DoubleListNode(int v) {
+            value = v;
+        }
+    }
+
+    //反转双链表
+    public DoubleListNode reverseDoubleList(DoubleListNode head) {
+        DoubleListNode pre = null;
+        DoubleListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = head.last;
+            head.last = next;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
 }
